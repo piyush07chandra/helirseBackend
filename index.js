9b5680598c1196351a86d35cd4384273af604649
piyush <piyushchandra9458@gmail.com>
2023-11-21 19:28:53 -1200

    updated

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ async function main() {
     available:"String"
 })
 const User = mongoose.model('User', userSchema);
-const  users = User.find({}).exec();
+const  users = User.find({}).timeout(100000);;
  
   app.get('/',(req,res)=>{
     res.send("these are users ",users)
